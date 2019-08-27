8f562dad3a6b252e0a8a1d625f4b337627f4787e
Małgorzata Osojca <mosojca@squiz.pl>
2019-08-27 16:24:45 +0200

    mobile version

---
+++ src/modules/events-block/js/global.js
@@ -0,0 +1,24 @@
+// slider
+$slick_slider = $('.events__boxes');
+settings_slider = {
+  infinite: true,
+  dots: true,
+  arrows: false
+  // more settings
+}
+slick_on_mobile( $slick_slider, settings_slider);
+
+// slick on mobile
+function slick_on_mobile(slider, settings){
+  $(window).on('load resize', function() {
+    if ($(window).width() > 768) {
+      if (slider.hasClass('slick-initialized')) {
+        slider.slick('unslick');
+      }
+      return
+    }
+    if (!slider.hasClass('slick-initialized')) {
+      return slider.slick(settings);
+    }
+  });
+};
