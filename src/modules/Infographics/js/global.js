cd1f34dc089e29e589ca2b44db24d80eb00e5eeb
Małgorzata Osojca <squiz@MacBook-Pro-squiz.local>
2019-08-29 14:54:27 +0200

    infographics changes

--- src/modules/Infographics/js/global.js
+++ src/modules/Infographics/js/global.js
@@ -6,11 +6,11 @@ settings_slider = {
   arrows: false
   // more settings
 }
-slick_on_mobile( $slick_slider, settings_slider);
+slick_on_mobile($slick_slider, settings_slider);
 
 // slick on mobile
-function slick_on_mobile(slider, settings){
-  $(window).on('load resize', function() {
+function slick_on_mobile(slider, settings) {
+  $(window).on('load resize', function () {
     if ($(window).width() > 768) {
       if (slider.hasClass('slick-initialized')) {
         slider.slick('unslick');
@@ -22,3 +22,37 @@ function slick_on_mobile(slider, settings){
     }
   });
 };
+
+
+// Scroll animation
+
+$.fn.moveIt = function () {
+  var $window = $(window);
+  var instances = [];
+
+  $(this).each(function () {
+    instances.push(new moveItItem($(this)));
+  });
+
+  window.addEventListener('scroll', function () {
+    var scrollTop = $window.scrollTop();
+    instances.forEach(function (inst) {
+      inst.update(scrollTop);
+    });
+  }, { passive: true });
+}
+
+var moveItItem = function (el) {
+  this.el = $(el);
+  this.speed = parseInt(this.el.attr('data-scroll-speed'));
+};
+
+moveItItem.prototype.update = function (scrollTop) {
+  this.el.css('transform', 'translateY(' + -(scrollTop / this.speed) + 'px)');
+};
+
+$(function () {
+  $('[data-scroll-speed]').moveIt();
+});
+
+
