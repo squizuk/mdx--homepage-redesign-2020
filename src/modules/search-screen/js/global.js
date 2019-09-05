18a165c721a49f6274a7859f260651c0806e0ea4
Małgorzata Osojca <squiz@MacBook-Pro-squiz.local>
2019-09-05 16:16:14 +0200

    search screen changes

--- src/modules/search-screen/js/global.js
+++ src/modules/search-screen/js/global.js
@@ -2,19 +2,42 @@ var mdxGlobal = new Bloodhound({
     datumTokenizer: Bloodhound.tokenizers.whitespace,
     queryTokenizer: Bloodhound.tokenizers.whitespace,
     local: [
-      "business",
-      "building",
-      "but",
-      "bus",
-      "build"
+        "business",
+        "building",
+        "but",
+        "bus",
+        "build"
     ]
-  });
+});
 
-  $('#multiple-datasets .typeahead').typeahead({
+var mdxSuggestion = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.whitespace,
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    local: [
+        "Business Information Systems Management with Integrated Placement (15 months/24 months) MSc",
+        "Building Information Modelling Management and Integrated Digital Delivery MSc/PGDip/PGCert",
+        "Building Information Modelling Management and Integrated Digital Delivery MSioiiop",
+        "Building Information Modelling Management and Integrated Digital Delityutyutyu",
+        "Building Information Modelling Management and Integrated Digital Delivery MSc/tyutyutyu",
+        
+    ]
+});
+
+$('#multiple-datasets .typeahead').typeahead({
     highlight: true
-  },
+},
     {
-      name: 'mdxGlobal',
-      source: mdxGlobal,
+        name: 'mdxGlobal',
+        source: mdxGlobal,
     },
-  );
+    {
+        name: 'mdxSuggestion',
+        source: mdxSuggestion,
+        templates: {
+            header: '<h3 class="search__suggestion-header">Courses</h3>',
+            suggestion: function (data) {
+                return '<div class="search__suggestion-component">' + '<p class="search__suggestion-text">' + data + '</p>' + '</div>'
+            }
+        }
+    }
+);
